ee10ff33e141af583be7db23e49a93297cd9f719
Gurneer Bedi <gurneer1223@gmail.com>
2024-11-28 19:31:39 -0500

    Added discover results page, prints list of books depending on user selected moods

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import DiscoverPage from "./pages/DiscoverPage";
+import DiscoverResultsPage from "./pages/DiscoverResultsPage";
 
 
 const App = ()  =>{
@@ -12,6 +13,7 @@ return (
             <Route path="/" element={<HomePage />}></Route>
             <Route path="/login" element={<LoginPage />}></Route>
             <Route path="/discover" element={<DiscoverPage />}></Route>
+            <Route path = "/discover/:moodQuery" element ={<DiscoverResultsPage/>}/>
         </Routes>
     </Router>
 );
---
+++ src/pages/DiscoverResultsPage.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import "./DiscoverResultsPage.scss";
+import { useState } from "react";
+import axios from "axios";
+
+const DiscoverResultsPage = () => {
+    const location = useLocation();
+    const books = location.state?.books || [];
+    const [updatedBooks, setUpdatedBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    console.log("Books:", books);
+
+    useEffect(() => {
+        const fetchDescriptions = async () => {
+            if (books.length === 0) return;
+            setLoading(true);
+            setError(null);
+
+            try {
+                const response = await axios.post(
+                    "http://localhost:3001/api/booksdescription",
+                    {
+                        books,
+                    }
+                );
+                console.log("Books with description:", response.data)
+                setUpdatedBooks(response.data);
+            } catch (error) {
+                console.error("Error fetching book descriptions:", error);
+                setError("Failed to fetch book descriptions");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchDescriptions();
+    }, [books]);
+
+    return (
+        <div className="discover-results-container">
+            <h2>Books Matching Your Mood</h2>
+            <div className="books-list">
+                {updatedBooks.length === 0 ? (
+                <p>No books found matching the selected moods.</p>) : (
+                <ul>
+                    {updatedBooks.map((book, index) => (
+                        <li key={index} className="book-item">
+                            <img
+                                src={
+                                    book.imageUrl || "bookimageplaceholder.jpg"
+                                }
+                                alt={book.Title}
+                                className="book-cover"
+                            />
+                            <h3>{book.Title}</h3>
+                            <p>{book.Author}</p>
+                            {/* <p>{book.description}</p> */}
+                        </li>
+                    ))}
+                </ul>
+                )}
+            </div>
+        </div>
+    );
+};
+export default DiscoverResultsPage;
